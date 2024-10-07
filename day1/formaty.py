18928a0e7febca213eba1f57ff4dda7cb42b0038
rjaniak <rajkonkret660@gmail.com>
2024-10-07 15:17:51 +0200

    [1.6] update formaty.py

--- day1/formaty.py
+++ day1/formaty.py
@@ -20,3 +20,47 @@ print("Witaj {}, masz teraz {} lat.".format(user, wiek))  # Witaj Tomek, masz te
 
 print(f"Witaj {user}, masz teraz {wiek} lat.")  # Witaj Tomek, masz teraz 39 lat.
 # f - fstring, tekst sformatowany
+
+print("Uzywamy wersji Pythona %i" % 3)  # Uzywamy wersji Pythona 3
+print("Uzywamy wersji Pythona %f" % 3)  # Uzywamy wersji Pythona 3.000000
+print("Uzywamy wersji Pythona %f" % 3.9)  # Uzywamy wersji Pythona 3.900000
+print("Uzywamy wersji Pythona %.2f" % 3.9)  # Uzywamy wersji Pythona 3.90
+print("Uzywamy wersji Pythona %.1f" % 3.9)  # Uzywamy wersji Pythona 3.9
+print("Uzywamy wersji Pythona %.0f" % 3.9)  # Uzywamy wersji Pythona 4 - zaokrągli
+print("Uzywamy wersji Pythona %.f" % 3.9)  # Uzywamy wersji Pythona 4
+print("Uzywamy wersji Pythona %.f" % 3.9 * 2)  # Uzywamy wersji Pythona 4Uzywamy wersji Pythona 4
+print("Uzywamy wersji Pythona %.f" % (3.9 * 2))  # Uzywamy wersji Pythona 8
+# zaokrągla przy wyświetlaniu
+print("Wynik = %.2f" % 3.876)  # Wynik = 3.88
+x = 3.14
+print("Zero miejsc po przecinku %.f" % x)
+print("X nadal się nie zmienia", x)
+# Zero miejsc po przecinku 3
+# X nadal się nie zmienia 3.14
+
+y = round(x)
+print("x=", x, "y=", y)  # x= 3.14 y= 3
+print(f"{x=}, {y=}")  # x= 3.14 y= 3
+x = 3.1416
+print(round(x, 2))  # 3.14
+
+print(f"Uzywamy wersji python {wersja}")  # Uzywamy wersji python 3.900001
+print(f"Uzywamy wersji python {wersja:.1f}")  # Uzywamy wersji python 3.9
+print(f"Uzywamy wersji python {wersja:.2f}")  # Uzywamy wersji python 3.90
+print(f"Uzywamy wersji python {wersja:.0f}")  # Uzywamy wersji python 4
+# print(f"Uzywamy wersji python {wersja:.f}")  # ValueError: Format specifier missing precision
+
+print(f"{user:>10}")  # "     Tomek"
+print(f"{user:<15}")  # "Tomek          "
+print(f"{user:^20}")  # "       Tomek        "
+
+print(liczba)  # 456123789567
+print(f"Nasza duża liczba {liczba:,}")  # Nasza duża liczba 456,123,789,567
+print(f"Nasza duża liczba {liczba:_}")  # Nasza duża liczba 456_123_789_567
+print(f"Nasza duża liczba {liczba:,}".replace(",", "."))  # Nasza duża liczba 456.123.789.567
+print(f"Nasza duża liczba {liczba:,}".replace(",", " "))  # Nasza duża liczba 456 123 789 567
+
+# 1500000000000
+liczba_2 = 1_500_000_000_000
+print(type(liczba_2))  # <class 'int'>
+print(liczba_2)  # 1500000000000
