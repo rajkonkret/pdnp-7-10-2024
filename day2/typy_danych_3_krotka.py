ab1bb2c862bb934895458cb56511c1adeeb0a7dd
rjaniak <rajkonkret660@gmail.com>
2024-10-08 12:29:43 +0200

    [2.11] update typy_danych_3_krotka.py

--- day2/typy_danych_3_krotka.py
+++ day2/typy_danych_3_krotka.py
@@ -27,3 +27,18 @@ print(tupla_liczby)  # (43, 55, 22.34, 11, 200)
 
 del tupla_liczby  # usunięcie całej tupli
 # print(tupla_liczby)  # NameError: name 'tupla_liczby' is not defined
+
+tupla_imiona = "Radek", "Tomek", 'Zenek', 'Marcin', "Ania", "Magda"
+print(tupla_imiona)  # ('Radek', 'Tomek', 'Zenek', 'Marcin', 'Ania', 'Magda')
+print(type(tupla_imiona))  # <class 'tuple'>
+
+print(tupla_imiona.index("Radek"))  # indeks 0
+print(tupla_imiona.count("Zenek"))  # wystepuje 1 raz
+
+print(len(tupla_imiona))  # długość tupli 6
+
+# sorted() - sortowanie kolekcji
+# sortowanie krotki zwróci nową listę
+print(sorted(tupla_imiona))  # ['Ania', 'Magda', 'Marcin', 'Radek', 'Tomek', 'Zenek']
+# nie zmieni samej krotki
+print(tupla_imiona)  # ('Radek', 'Tomek', 'Zenek', 'Marcin', 'Ania', 'Magda')
