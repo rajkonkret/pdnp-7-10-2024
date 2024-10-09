6098498a0db5d0582fde845d398c716370dae7ec
rjaniak <rajkonkret660@gmail.com>
2024-10-09 15:07:56 +0200

    [3.17] add kalkulator.py

---
+++ day3/kalkulator.py
@@ -0,0 +1,6 @@
+# napisac program kalkulator z wykorzystaniem pętli while True
+# menu z dostępnymi opcjami (=,-,*,/, Wyjscie z programu) print("""""")
+# podać wybrana opcje
+# pobrać liczby do obliczeń
+# wyswietlic wynik działania (f string)
+# obsłużyć wyjątki
