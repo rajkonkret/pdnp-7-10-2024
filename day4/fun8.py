1ea27f395566f08c4d13c3979d5e7799d7d5898d
rjaniak <rajkonkret660@gmail.com>
2024-10-10 14:32:29 +0200

    [4.14] update fun8

--- day4/fun8.py
+++ day4/fun8.py
@@ -17,3 +17,37 @@ print(wiek(10))  # nastolatek
 print(wiek(17))  # nastolatek
 print(wiek(18))  # dorosły
 print(wiek(25))  # dorosły
+
+# mapowanie
+# pomnożyc kazdy element przez 2 i wypisac
+lista = [1, 2, 3, 10, 20, 30, 60, 100, 300, 500]
+lista_wyn = []
+for i in lista:
+    lista_wyn.append(i * 2)
+
+print(lista_wyn)  # [2, 4, 6, 20, 40, 60, 120, 200, 600, 1000]
+print([i * 2 for i in lista])  # [2, 4, 6, 20, 40, 60, 120, 200, 600, 1000]
+
+lista_wyn_2 = []
+
+
+def zmien(x):
+    return x * 2
+
+
+for i in lista:
+    lista_wyn_2.append(zmien(i))
+
+print(lista_wyn_2)  # [2, 4, 6, 20, 40, 60, 120, 200, 600, 1000]
+
+# map() -  funkcje wyzszego rzędu
+print(f"Zastosowanie map(): {list(map(zmien, lista))}")
+# Zastosowanie map(): [2, 4, 6, 20, 40, 60, 120, 200, 600, 1000]
+
+# zastosowanie lambdy jako funkcji anonimowej
+print(f"Zastosowanie map(): {list(map(lambda x: x * 2, lista))}")
+# Zastosowanie map(): [2, 4, 6, 20, 40, 60, 120, 200, 600, 1000]
+print(f"Zastosowanie map(): {list(map(lambda x: x * 4, lista))}")
+print(f"Zastosowanie map(): {list(map(lambda x: x * 14, lista))}")
+# Zastosowanie map(): [4, 8, 12, 40, 80, 120, 240, 400, 1200, 2000]
+# Zastosowanie map(): [14, 28, 42, 140, 280, 420, 840, 1400, 4200, 7000]
