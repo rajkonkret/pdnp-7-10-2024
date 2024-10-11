b604564df0530fc3319d8e47132606267bd23773
rjaniak <rajkonkret660@gmail.com>
2024-10-11 15:03:17 +0200

    [5.9] add baza2.py

---
+++ day5/baza2.py
@@ -0,0 +1,44 @@
+# sqlite
+import sqlite3
+
+try:
+    # conn = sqlite3.connect('baza_danych.db')
+    conn = sqlite3.connect(':memory:')
+    c = conn.cursor()
+    print("Baza została podłączona")
+
+    query = '''
+    CREATE TABLE IF NOT EXISTS developers (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    salary REAL NOT NULL);
+    '''
+    # REAL - -> float
+
+    c.execute(query)
+    conn.commit()
+
+    insert = """
+    INSERT INTO developers (id,name,salary) VALUES (1,'Radek','10000');
+    """
+    c.execute(insert)
+    conn.commit()
+
+    select = """
+    SELECT * FROM developers;
+    """
+    for row in c.execute(select):
+        print(row) # (1, 'Radek', 10000.0)
+
+except sqlite3.Error as e:
+    print("Bład bazy", e)
+finally:
+    if conn:
+        conn.close()
+        print("Połaczenie zostało zamknięte")
+
+# Baza została podłączona
+# Połaczenie zostało zamknięte
+# Baza została podłączona
+# (1, 'Radek', 10000.0)
+# Połaczenie zostało zamknięte
